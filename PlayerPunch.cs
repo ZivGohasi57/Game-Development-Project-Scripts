3c4d8caeda806be2452f947cd63604743a4ed25c
ZivGohasi57 <ziv.gohasi572@gmail.com>
2024-12-05 16:44:03 +0200

    Update PlayerPunch.cs

--- PlayerPunch.cs
+++ PlayerPunch.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerPunch : MonoBehaviour
 {
-    public float punchRange = 2.5f;  // הטווח של האגרוף
-    public LayerMask breakableLayer;  // שכבת אובייקטים שניתן לשבור (כגון הכד)
+    public float punchRange = 2.5f; 
+    public LayerMask breakableLayer; 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))  // לחץ על Q לאגרוף
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             Punch();
         }
@@ -17,27 +17,20 @@ public class PlayerPunch : MonoBehaviour
 
     void Punch()
     {
-        // שליחת קרן קדימה מהשחקן לזיהוי התנגשות
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, punchRange, breakableLayer))
         {
-            Debug.Log("פגעת באובייקט שניתן לשבור!");
-            BreakObject(hit.collider.gameObject);  // קריאה לפונקציה ששוברת את האובייקט
-        }
-        else
-        {
-            Debug.Log("לא פגעת בכלום.");
+            BreakObject(hit.collider.gameObject); 
         }
     }
 
     void BreakObject(GameObject pot)
     {
-        // מחליף את הכד השלם בכד השבור
-        pot.SetActive(false);  // הסתרת הכד השלם
+        pot.SetActive(false);  
         Transform brokenPot = pot.transform.parent.Find("BrokenPot");
         if (brokenPot != null)
         {
-            brokenPot.gameObject.SetActive(true);  // הצגת הכד השבור
+            brokenPot.gameObject.SetActive(true);  
         }
     }
 }
