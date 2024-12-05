a0544dfbf3af185525eabb54a04488b8ce10784a
ZivGohasi57 <ziv.gohasi572@gmail.com>
2024-12-05 16:19:06 +0200

    Update KnightBehaviourScript.cs

--- KnightBehaviourScript.cs
+++ KnightBehaviourScript.cs
@@ -11,7 +11,6 @@ public class KnightBehaviourScript : MonoBehaviour
     LineRenderer line;
     public GameObject Point1;
     public GameObject Point2;
-    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -20,7 +19,6 @@ public class KnightBehaviourScript : MonoBehaviour
         line = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.transform.position, transform.position);
@@ -41,32 +39,25 @@ public class KnightBehaviourScript : MonoBehaviour
         if (!agent.isStopped && distance < 2)
         {
                 agent.isStopped = true;
-            animator.SetInteger("State", 0); //animation idle
+                animator.SetInteger("State", 0); //animation idle
 
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
-
-            // finds path to the target and starts moving towards the target
-            // Uses A* pfathfinding algorithm
             if (agent.isStopped)
             {
                 animator.SetInteger("State", 1); //walking
                 agent.SetDestination(target.transform.position);
                 agent.isStopped = false;
-                // set length of array of corners/line and array of path corners itself
                 line.positionCount = agent.path.corners.Length;
                 line.SetPositions(agent.path.corners);
             }
         }
-        // starts pathfinding with A* which is not efficient but
-        // is necessary to dynamically change the path
-        // 1. finds path to target
-        // 2. starts moving to the target
-        if(!agent.isStopped) { 
+
+        if(!agent.isStopped) 
+        { 
             agent.SetDestination(target.transform.position);
             agent.isStopped = false;
-            // set length of array of corners/line and array of path corners itself
             line.positionCount = agent.path.corners.Length;
             line.SetPositions(agent.path.corners);
 
