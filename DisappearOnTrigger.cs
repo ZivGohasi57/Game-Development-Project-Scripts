723def80e6be78d2197470247592de7438e6e4f6
ZivGohasi57 <ziv.gohasi572@gmail.com>
2024-12-05 15:55:21 +0200

    Update DisappearOnTrigger.cs

--- DisappearOnTrigger.cs
+++ DisappearOnTrigger.cs
@@ -10,24 +10,19 @@ public class DisappearOnTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // נגן את הצליל לפני שהאובייקט נעלם
             if (audio != null)
             {
                 audio.Play();
             }
 
-            // הפוך את האובייקט לבלתי נראה אחרי שהשמע התחיל
             StartCoroutine(DisableAfterSound());
         }
     }
 
-    // קורוטינה שמחכה עד שהשמע ינוגן לפני שהאובייקט נעלם
     private IEnumerator DisableAfterSound()
     {
-        // חכה למשך הזמן של הצליל
         yield return new WaitForSeconds(audio.clip.length);
 
-        // האובייקט נעלם
         gameObject.SetActive(false);
     }
 }
