41a08d41517d95b1748428fe9152d93702765f12
ZivGohasi57 <ziv.gohasi572@gmail.com>
2024-12-05 15:54:56 +0200

    Update DisappearAndAppearOnTrigger.cs

--- DisappearAndAppearOnTrigger.cs
+++ DisappearAndAppearOnTrigger.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class AppearAndDisappearOnTrigger : MonoBehaviour
 {
-    public GameObject objectToDisappear; // האובייקט שצריך להיעלם
-    public AudioSource appearSound; // מקור השמע עבור הצליל שיופיע
+    public GameObject objectToDisappear;
+    public AudioSource appearSound; 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // האובייקט הנוכחי יופיע (מופעל מחדש)
+           
             gameObject.SetActive(true);
 
-            // האובייקט האחר ייעלם
             if (objectToDisappear != null)
             {
                 objectToDisappear.SetActive(false);
             }
 
-            // נגן את צליל ההופעה
             if (appearSound != null)
             {
                 appearSound.Play();
